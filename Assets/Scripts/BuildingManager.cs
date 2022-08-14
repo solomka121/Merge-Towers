8599f93aed5b6fc6833198315b0aea9652e51da2
Slava <slavasolomahin12@gmail.com>
2022-08-14 13:17:58 +0300

    Grid Indicators + Text Mesh Pro

--- Assets/Scripts/BuildingManager.cs
+++ Assets/Scripts/BuildingManager.cs
@@ -1,13 +1,81 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class BuildingManager : MonoBehaviour
 {
     public Vector2Int gridSize;
+    [SerializeField] private Transform _gridCellsParent;
+    [SerializeField] private GridCell _gridCellPrefab;
 
     private Building[,] _grid;
+    private GridCell[,] _gridCells;
+
+    private List<Building> _buildings;
+    private List<Vector2Int> _freePositions;
+
+    private Building _selectedBuilding;
 
     private void Awake()
     {
         _grid = new Building[gridSize.x, gridSize.y];
+        _gridCells = new GridCell[gridSize.x, gridSize.y];
+
+        _buildings = new List<Building>();
+        _freePositions = new List<Vector2Int>();
+        for(int x = 0; x < gridSize.x; x++)
+        {
+            for(int y = 0; y < gridSize.y; y++)
+            {
+                _freePositions.Add(new Vector2Int(x, y));
+            }
+        }
+
+        SpawnCells();
+    }
+
+    private void SpawnCells()
+    {
+        for (int x = 0; x < gridSize.x; x++)
+        {
+            for (int y = 0; y < gridSize.y; y++)
+            {
+                GridCell gridCell = Instantiate(_gridCellPrefab, _gridCellsParent);
+                gridCell.transform.localPosition = new Vector3(x, _gridCellsParent.transform.position.y, y);
+                gridCell.SetAvailable(true);
+                _gridCells[x, y] = gridCell;
+            }
+        }
+    }
+
+    public void SpawnBuilding(Building building)
+    {
+        if (IsThereEmptySpace() == false)
+            return;
+
+        Building spawnedBuilding = Instantiate(building);
+        Vector2Int position = GetRandomCoordinates();
+        PlaceBuilding(spawnedBuilding , position);
+    }
+
+    public void PlaceBuilding(Building building , Vector2Int position)
+    {
+        _freePositions.Remove(position);
+        _buildings.Add(building);
+        _grid[position.x, position.y] = building;
+
+        _gridCells[position.x, position.y].SetAvailable(false);
+
+        building.transform.position = transform.position + new Vector3(position.x , transform.position.y, position.y);
+    }
+
+    public Vector2Int GetRandomCoordinates()
+    {
+        int randomFreePositionIndex = Random.Range(0, _freePositions.Count);
+        return _freePositions[randomFreePositionIndex];
+    }
+
+    public bool IsThereEmptySpace()
+    {
+        return _freePositions.Count > 0;
     }
 }
---
+++ Assets/Scripts/GridCell.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridCell : MonoBehaviour
+{
+    [SerializeField] private MeshRenderer _meshRenderer;
+    [SerializeField] private Color _available = Color.green;
+    [SerializeField] private Color _notAvailable = Color.red;
+    [SerializeField] private Color _selected = Color.cyan;
+
+    public void SetAvailable(bool available)
+    {
+        SetVisible(true);
+
+        if (available)
+        {
+            _meshRenderer.material.color = _available;
+        }
+        else
+        {
+            _meshRenderer.material.color = _notAvailable;
+        }
+    }
+
+    public void SetSelected(bool selected)
+    {
+        if (selected)
+        {
+            _meshRenderer.material.color = _selected;
+        }
+        else
+        {
+            SetVisible(false);
+        }
+    }
+
+    public void SetVisible(bool visible)
+    {
+        _meshRenderer.gameObject.SetActive(visible);
+    }
+}
